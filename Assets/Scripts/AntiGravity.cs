3eaed4bb2e2ce2816e1b25dab2f17cb3f382314f
Emirhan Ekşi <aim.emirhan@gmail.com>
2024-10-05 00:16:31 +0300

    fifty

---
+++ Assets/Scripts/AntiGravity.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AntiGravity : MonoBehaviour
+{
+  
+    public Vector3 gravity = new Vector3(0, -0.5f, 0); // Sıfıra yakın bir yer çekimi kuvveti
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.useGravity = false; // Unity'nin yer çekimi etkisini devre dışı bırakıyoruz
+    }
+
+    void FixedUpdate()
+    {
+        rb.AddForce(gravity * rb.mass); // Düşük yer çekimi kuvveti uyguluyoruz
+    }
+}
